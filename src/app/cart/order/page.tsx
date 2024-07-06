5c072bbc0871067942499c62af66e02755cf52f4
bach9w <78446671+bach9w@users.noreply.github.com>
2024-07-06 16:51:03 +0300

    update

--- src/app/cart/order/page.tsx
+++ src/app/cart/order/page.tsx
@@ -17,6 +17,7 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 import { models } from "@/utils/constants";
+import { Suspense } from "react";
 
 const Order = () => {
 	const router = useRouter();
@@ -28,79 +29,81 @@ const Order = () => {
 	const symbol = searchParams.get("symbol");
 
 	return (
-		<MaxWidthWrapper>
-			<h1 className="bg-black text-white rounded w-full flex items-center justify-center mt-10">
-				Разгледайте поръчката преди да продължите към плащане
-			</h1>
-			<Card className="mt-4">
-				<CardHeader className="w-full flex items-center justify-center">
-					<Badge className="w-full">Преглед на поръчка</Badge>
-				</CardHeader>
-				<CardContent className="grid grid-cols-1 xl:grid-cols-2">
-					{gerb !== "undefined" && (
-						<div className="w-full h-[40px] flex items-center justify-center">
-							Избрали сте герб - {gerb}
-						</div>
-					)}
-					{flag !== "undefined" && (
-						<div className="w-full h-[40px] flex items-center justify-center">
-							Избрали сте флаг - {flag}
-						</div>
-					)}
-					{text && (
-						<div className="w-full h-[40px] flex items-center justify-center">
-							Избрали сте текстов надпис - {text}
-						</div>
-					)}
-					{table && (
-						<div className="w-full h-[40px] flex items-center justify-center">
-							Избрали сте надпис върху табелка - {table}
-						</div>
-					)}
-					{symbol && (
-						<div className="w-full h-[40px] flex items-center justify-center">
-							Избрали сте символ върху калъфа - {symbol}
-						</div>
-					)}
-				</CardContent>
-				<CardFooter>
-					{gerb === "undefined" &&
-					flag === "undefined" &&
-					!text &&
-					!table &&
-					!symbol ? (
-						<div className="w-full h-[40px] flex flex-col items-center justify-center">
-							Избрали сте поръчка без допълнителни детайли
-							<Button
-								onClick={() => {
-									router.push("/cart/");
-								}}
-							>
-								Върнете се обратно
-							</Button>
-						</div>
-					) : (
-						<div className="w-full flex flex-col gap-4">
-							<Select>
-								<SelectTrigger className="w-full">
-									<SelectValue placeholder="Модел на телефона" />
-								</SelectTrigger>
-								<SelectContent>
-									{models.map((s) => (
-										<SelectItem key={s.id} value={s.name}>
-											{s.name}
-										</SelectItem>
-									))}
-								</SelectContent>
-							</Select>
-							<Button variant="outline" className="w-full rounded-none">
-								Плати
-							</Button>
-						</div>
-					)}
-				</CardFooter>
-			</Card>
-		</MaxWidthWrapper>
+		<Suspense>
+			<MaxWidthWrapper>
+				<h1 className="bg-black text-white rounded w-full flex items-center justify-center mt-10">
+					Разгледайте поръчката преди да продължите към плащане
+				</h1>
+				<Card className="mt-4">
+					<CardHeader className="w-full flex items-center justify-center">
+						<Badge className="w-full">Преглед на поръчка</Badge>
+					</CardHeader>
+					<CardContent className="grid grid-cols-1 xl:grid-cols-2">
+						{gerb !== "undefined" && (
+							<div className="w-full h-[40px] flex items-center justify-center">
+								Избрали сте герб - {gerb}
+							</div>
+						)}
+						{flag !== "undefined" && (
+							<div className="w-full h-[40px] flex items-center justify-center">
+								Избрали сте флаг - {flag}
+							</div>
+						)}
+						{text && (
+							<div className="w-full h-[40px] flex items-center justify-center">
+								Избрали сте текстов надпис - {text}
+							</div>
+						)}
+						{table && (
+							<div className="w-full h-[40px] flex items-center justify-center">
+								Избрали сте надпис върху табелка - {table}
+							</div>
+						)}
+						{symbol && (
+							<div className="w-full h-[40px] flex items-center justify-center">
+								Избрали сте символ върху калъфа - {symbol}
+							</div>
+						)}
+					</CardContent>
+					<CardFooter>
+						{gerb === "undefined" &&
+						flag === "undefined" &&
+						!text &&
+						!table &&
+						!symbol ? (
+							<div className="w-full h-[40px] flex flex-col items-center justify-center">
+								Избрали сте поръчка без допълнителни детайли
+								<Button
+									onClick={() => {
+										router.push("/cart/");
+									}}
+								>
+									Върнете се обратно
+								</Button>
+							</div>
+						) : (
+							<div className="w-full flex flex-col gap-4">
+								<Select>
+									<SelectTrigger className="w-full">
+										<SelectValue placeholder="Модел на телефона" />
+									</SelectTrigger>
+									<SelectContent>
+										{models.map((s) => (
+											<SelectItem key={s.id} value={s.name}>
+												{s.name}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+								<Button variant="outline" className="w-full rounded-none">
+									Плати
+								</Button>
+							</div>
+						)}
+					</CardFooter>
+				</Card>
+			</MaxWidthWrapper>
+		</Suspense>
 	);
 };
 
