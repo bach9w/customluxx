8aa687e4dd626841bf8ddbbc4bc7ee2c0ab1d551
bach9w <78446671+bach9w@users.noreply.github.com>
2024-08-19 18:35:01 +0300

    upp

--- src/app/cart/products/orderModal/component.tsx
+++ src/app/cart/products/orderModal/component.tsx
@@ -4,60 +4,67 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
-import { MdCheck, MdCheckCircle } from "react-icons/md";
+import { MdCheck } from "react-icons/md";
 import { motion } from "framer-motion";
+import { useCallback } from "react";
 
-const Modal = ({
-	status,
-	setStatus,
-	products,
-}: {
+type Product = {
+	id: string;
+	image: string;
+};
+
+interface ModalProps {
 	status: boolean;
 	setStatus: (value: boolean) => void;
-	products: any;
-}) => {
+	products: Product;
+}
+
+const Modal = ({ status, setStatus, products }: ModalProps) => {
 	const { id, image } = products;
+
+	const handleClose = useCallback(() => {
+		setStatus(!status);
+	}, [status, setStatus]);
+
+	const handleOrder = useCallback(() => {
+		// Логика за поръчка
+		setStatus(!status);
+	}, [status, setStatus]);
+
 	return (
 		<motion.div
 			initial={{ opacity: 0, scale: 0.7 }}
 			animate={{ opacity: 1, scale: 1 }}
 			transition={{ ease: "easeOut", duration: 0.2 }}
-			className="h-screen bg-white min-w-[100vw] w-full"
+			className="h-screen bg-white w-full"
 		>
 			{id && image && (
-				<div className="w-full h-full ">
-					<div className="flex justify-start gap-10 flex-col items-center h-full w-full">
-						<Badge className="w-full h-20 rounded-none text-xl md:text-3xl flex justify-center">
-							ПОРЪЧАЙ СЕГА ЗА 45 ЛВ
-						</Badge>
-						<div className="uppercase">
-							<Label className="text-md">Добави своето име</Label>
-							<Input
-								placeholder={"Иван"}
-								className="min-w-[80vw] text-[17px]"
-							/>
-							<Label className="text-md">Текст под символи</Label>
-							<Input
-								placeholder={"Иван"}
-								className="min-w-[80vw] text-[17px]"
-							/>
-						</div>
-						<Image
-							className="rounded-xl"
-							src={`/${image}.jpg`}
-							alt=""
-							width={300}
-							height={300}
-						/>
+				<div className="flex flex-col items-center h-full w-full gap-6 p-4">
+					<Badge className="w-full h-20 rounded-none text-xl md:text-3xl flex justify-center items-center">
+						ПОРЪЧАЙ СЕГА ЗА 45 ЛВ
+					</Badge>
+					<div className="uppercase">
+						<Label className="text-md">Добави своето име</Label>
+						<Input placeholder={"Иван"} className="min-w-[80vw] text-[17px]" />
+						<Label className="text-md">Текст под символи</Label>
+						<Input placeholder={"Иван"} className="min-w-[80vw] text-[17px]" />
 					</div>
+					<Image
+						className="rounded-xl"
+						src={`/${image}.jpg`}
+						alt="Product image"
+						width={300}
+						height={300}
+						objectFit="cover"
+						blurDataURL={`blur(5px ${image}.jpg)`}
+					/>
 				</div>
 			)}
-			<div className="fixed grid grid-cols-2 bottom-0 h-20 w-full bg-black ">
+			<div className="fixed grid grid-cols-2 bottom-0 h-20 w-full bg-black">
 				<Button
+					aria-label="Закриване"
 					className="w-full h-full rounded-none hover:bg-red-800"
-					onClick={() => {
-						setStatus(!status);
-					}}
+					onClick={handleClose}
 				>
 					Закриване
 					<svg
@@ -76,10 +83,9 @@ const Modal = ({
 					</svg>
 				</Button>
 				<Button
+					aria-label="Поръчай сега"
 					className="w-full h-full rounded-none hover:bg-sky-500"
-					onClick={() => {
-						setStatus(!status);
-					}}
+					onClick={handleOrder}
 				>
 					Поръчай сега
 					<MdCheck size={20} />
--- src/app/cart/products/page.tsx
+++ src/app/cart/products/page.tsx
@@ -38,7 +38,7 @@ const Experimental = () => {
 	};
 
 	return (
-		<AnimatePresence>
+		<AnimatePresence mode="wait">
 			<div>
 				{MODAL_STATUS && (
 					<div>
@@ -53,8 +53,8 @@ const Experimental = () => {
 
 			<div className="w-full">
 				<div className="min-h-screen h-full">
-					<div className="header text-3xl font-bold uppercase text-white w-full flex items-center justify-center">
-						Products
+					<div className="header text-3xl font-bold uppercase text-white w-full flex items-center justify-center mt-4 mb-4">
+						Продукти
 					</div>
 					<div className="h-full">
 						<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 px-4">
@@ -78,45 +78,104 @@ const Experimental = () => {
 											/>
 											<div
 												onClick={() => toggleExpand(i._id)}
-												className="relative bottom-[30px] w-full1 z-50 hover:bg-white/40 uppercase font-bold justify-center"
+												className="relative bottom-[20px] rounded-b-xl bg-red-500 text-white flex w-full1 z-50 hover:bg-white/40 uppercase font-bold justify-center"
 											>
-												<ArrowDown />
-												{expandedProducts[i._id] && (
-													<motion.div
-														className="shadow-md drop-shadow-md absolute rounded-b-lg bg-white z-50 w-full h-[100px] flex items-center justify-center flex-col"
-														initial={{ opacity: 0, y: 100 }}
-														animate={{ opacity: 1, y: 0 }}
-														exit={{
-															opacity: 0,
-															y: -100,
-															transition: { duration: 0.5 },
-														}}
-													>
-														<div className="grid grid-cols-1 md:grid-cols-2">
-															<p>Цена - 45 лв.</p>
-															<p>Изработка - до 2 дни</p>
-														</div>
-														<div className="grid grid-cols-1 w-full gap-2 p-1">
-															<Button
-																onClick={() => {
-																	SETMODAL_STATUS(true);
-																	setProduct({
-																		id: i._id,
-																		image: i.img,
-																	});
-																	scrollTo({
-																		left: 0,
-																		top: 0,
-																		behavior: "smooth",
-																	});
-																}}
-																className="w-full"
-															>
-																Изработи сега
-															</Button>
-														</div>
-													</motion.div>
-												)}
+												ИЗБЕРИ
+												<AnimatePresence mode="wait">
+													{expandedProducts[i._id] && (
+														<motion.div
+															className="shadow-lg drop-shadow-lg absolute rounded-b-lg bg-black z-50 w-full h-[120px] flex items-center justify-center flex-col p-4 md:h-[150px] "
+															initial={{ opacity: 0, y: 100 }}
+															animate={{ opacity: 1, y: -95 }}
+															exit={{
+																opacity: 0,
+																y: 100,
+																transition: { duration: 0.5 },
+															}}
+														>
+															<div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+																<div className="flex items-center gap-2 justify-center">
+																	<svg
+																		xmlns="http://www.w3.org/2000/svg"
+																		className="h-6 w-6 text-red-500"
+																		fill="none"
+																		viewBox="0 0 24 24"
+																		stroke="currentColor"
+																	>
+																		<path
+																			strokeLinecap="round"
+																			strokeLinejoin="round"
+																			strokeWidth={2}
+																			d="M12 8v4l3 3m-3-3V8m9 4a9 9 0 11-18 0 9 9 0 0118 0z"
+																		/>
+																	</svg>
+																	<p className="text-lg font-bold text-white">
+																		Изработка:{" "}
+																		<span className="text-red-500">
+																			до 2 дни
+																		</span>
+																	</p>
+																</div>
+																<div className="flex items-center gap-2 justify-center">
+																	<svg
+																		xmlns="http://www.w3.org/2000/svg"
+																		className="h-6 w-6 text-red-500"
+																		fill="none"
+																		viewBox="0 0 24 24"
+																		stroke="currentColor"
+																	>
+																		<path
+																			strokeLinecap="round"
+																			strokeLinejoin="round"
+																			strokeWidth={2}
+																			d="M8 12h12M8 12l4 4m-4-4l4-4"
+																		/>
+																	</svg>
+
+																	<p className="text-lg font-bold text-white">
+																		Цена:{" "}
+																		<span className="text-red-500">45 лв.</span>
+																	</p>
+																</div>
+															</div>
+															<div className="grid grid-cols-1 w-full gap-2 mt-4">
+																<Button
+																	onClick={() => {
+																		SETMODAL_STATUS(true);
+																		setProduct({
+																			id: i._id,
+																			image: i.img,
+																		});
+																		scrollTo({
+																			left: 0,
+																			top: 0,
+																			behavior: "smooth",
+																		});
+																	}}
+																	className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition-colors duration-300"
+																>
+																	<span className="flex items-center justify-center gap-2">
+																		<svg
+																			xmlns="http://www.w3.org/2000/svg"
+																			className="h-5 w-5"
+																			fill="none"
+																			viewBox="0 0 24 24"
+																			stroke="currentColor"
+																		>
+																			<path
+																				strokeLinecap="round"
+																				strokeLinejoin="round"
+																				strokeWidth={2}
+																				d="M12 4v16m8-8H4"
+																			/>
+																		</svg>
+																		Изработи сега
+																	</span>
+																</Button>
+															</div>
+														</motion.div>
+													)}
+												</AnimatePresence>
 											</div>
 										</Card>
 									</motion.div>
